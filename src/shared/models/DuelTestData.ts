3187e656341f4f9a2f6022cde046477656add71e
Storken <magnushuttu@gmail.com>
2019-08-05 01:28:39 +0200

    new design for duels

--- src/shared/models/DuelTestData.ts
+++ src/shared/models/DuelTestData.ts
@@ -2,7 +2,7 @@ import Duel from './Duel';
 
 export const testDuels: Duel[] = [
     // tslint:disable-next-line:max-line-length
-    { duelId: 1, wiz1Id: 72, wiz2Id: 82, startBlock: 8262503, endBlock: 8262507, isAscensionBattle: true, moveSet1: [1, 3, 2], moveSet2: [1, 2, 3], power1: '320000000000000', power2: '620000000000000', timedOut: true },
+    { duelId: 1, wiz1Id: 72, wiz2Id: 82, startBlock: 8262503, endBlock: 8262507, isAscensionBattle: true, moveSet1: [1, 3, 2], moveSet2: [3, 2, 3], power1: '320000000000000', power2: '620000000000000', timedOut: true },
     // tslint:disable-next-line:max-line-length
     { duelId: 2, wiz1Id: 72, wiz2Id: 5000, startBlock: 8262503, endBlock: 8262507, isAscensionBattle: true, moveSet1: [1, 3, 2], moveSet2: [1, 2, 3], power1: '320000000000000', power2: '620000000000000', timedOut: true },
     // tslint:disable-next-line:max-line-length
